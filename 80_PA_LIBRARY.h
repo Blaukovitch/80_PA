31dbec18e31fad1189e8c363a02562fe1a7c20e6
Blaukovitch <116763547+Blaukovitch@users.noreply.github.com>
2022-10-26 14:08:33 -0700

    Create 80_PA_LIBRARY.h
    80_PA game library submit

---
+++ 80_PA_LIBRARY.h
@@ -0,0 +1,157 @@
+>>> from 80_PA_LIBRARY.h <<<
+> struct SC_KEY_KIT <   
+    Epic Mickey 2: The Power of Two
+    Grand Theft Auto IV
+    Ys Seven
+    Adam's Venture
+    Battle: Los Angeles
+    Cargo! The Quest For Gravity
+    Lego Star Wars III: The Clone Wars
+    Men of War: Assault Squad
+    Test Drive Unlimited 2
+    Yars' Revenge
+    Batman: Arkham City
+    Batman: Arkham City GOTY
+    Arcania: Gothic 4
+    Alone in the Dark: Near Death Investigation
+    Blood Bowl (Legendary Edition)
+    Dirt 3
+    Fable III
+    Far Cry 2
+    Ghostbusters: The Video Game
+    TRON: Evolution
+    Medal of Honor 2010
+    Doctor Who: The Adventure Games
+    Sir Pudding Wiggly
+    Split Second: Velocity
+    BoneCraft: The Video Game
+    BioShock 2
+    Final Fantasy VII
+    BioShock
+    The Travels of Marco Polo
+    Ys Foliage Ocean in Celceta
+    Grand Theft Auto IV: Episodes from Liberty City
+    Bionic Commando
+    ArmA 2: Operation Arrowhead
+    Overlord II
+    Sacred 2: Fallen Angel
+    Borderlands GOTY
+    Dexter: The Game
+    Spore
+    Mass Effect
+    Command & Conquer: Red Alert 3
+    BoneTown
+    J.U.L.I.A.
+    TRON: Evolution (RUSSIAN)
+    Burnout Paradise: The Ultimate Box
+    Mercenaries 2: World in Flames
+    Mirror’s Edge
+    The Godfather II
+    Manhunt 2
+    FUEL
+    cdBook (Latin)
+    cdBook (Greek)
+    Nancy Drew: The Silent Spy
+    Tale of Wuxia: Prequel
+    Air Raid - This Is Not a Drill!
+    Panzer Killer!
+    Red Shark
+    The Dark Eye: Chains of Satinav
+    Dead Space
+    Operation Flashpoint: Red River
+    Lego Pirates of the Caribbean: The Video Game
+    Wolfenstein (2009)
+    Brave: The Video Game
+    Pro Evolution Soccer 2014
+    Pro Evolution Soccer 2013
+    Command & Conquer 3: Tiberium Wars
+    Command & Conquer 3: Kane's Wrath
+    Need for Speed: ProStreet
+    The Sims 3
+    DENUVO Insider
+    DENUVO Introduction
+    Bone: Out from Boneville
+    Sam & Max: Culture Shock
+    Sam & Max: Ice Station Santa
+    Sam & Max Episode 2 - Moai Better Blues
+    Sam & Max Episode 3 - Night of the Raving Dead
+    Sam & Max Episode 4 - Chariots of the Dogs
+    Sam & Max Episode 5 - What's New, Beelzebub?
+    Monkey Island: The Trial and Execution of Guybrush Threepwood
+    Monkey Island: Launch of the Screaming Narwhal
+    Monkey Island: The Siege of Spinner Cay
+    S.T.A.L.K.E.R.: Shadow of Chernobyl
+    Sam & Max|Sea.1/Ep.3: The Mole, The Mob, and the Meatball
+    Sam & Max|Sea.1/Ep.5: Reality 2.0
+    Sam & Max|Sea.1/Ep.6: Bright Side of the Moon
+    Sam & Max|Sea.3/Ep.1: The Devil's Playhouse
+    Sam & Max|Sea.3/Ep.4: Beyond the Alley of the Dolls
+    Bone: The Great Cow Race
+    Monkey Island: Lair of the Leviathan
+    Strong Bad|Episode 5: 8-Bit Is Enough
+    Strong Bad|Episode 3: Baddest of the Bands
+    Strong Bad|Episode 4: Dangeresque 3
+    Strong Bad|Episode 1: Homestar Ruiner
+    Strong Bad|Episode 2: Strong Badia the Free
+    Telltale Texas Hold'em
+    Dark Messiah of Might and Magic
+    Divinity II: Flames of Vengeance
+    Divinity II: The Dragon Knight Saga
+    Cities XL 2011
+    Tomb Raider: Underworld
+    Race Driver: GRID
+    FIFA Manager 09
+    Hotel Giant 2
+    Puzzle Quest: Galactrix
+    NBA 2K10
+    DSA: Drakensang
+    Neverwinter Nights 2 (Gold)
+    Star Wars: The Force Unleashed II
+    A Farewell To Dragons (preview)
+    King's Bounty - Armored Princess (preview)
+    Men of War: Red Tide (preview)
+    NecroVisioN: Lost Company (preview)
+    Real Warfare: 1242 (preview)
+    Reign: Conflict of Nations (preview)
+    Stealth Force 2 - PC
+    Where's Waldo?
+    Mahjong Wisdom
+    Dark Void
+    High School Dreams
+    Crysis
+    Crysis Warhead
+    Age of Booty
+    Flock!
+    Dark Void Zero
+    Creat Campaning Strategy II
+    phase-6
+    18 Wheels of Steel: Across America (2003)
+    Toy Defense 2
+    Empire Earth II: The Art of Supremacy
+    Blur
+    Mount & Blade: Warband
+    Cars Toon: Mater's Tall Tales
+    Conflict: Denied Ops
+    The Sims 2 Shopping Edition
+    Stronghold Crusader
+    My Golf Game VTree
+    FIFA 11
+    International Basketball Manager Season 2010-2011
+    2K Sports Major League Baseball 2K12
+    Adobe Director 11.5 (A0102084926-0101)
+    Basisinformationen Uber Wertpapieren und weitere Kapitalanlagen
+    Adobe Flash Player 10.0 (A010164838-A511)
+    More! 4 CD-ROM
+    Vedere la tecnologia
+    Interaktive Sprachreise - Sprachkurs 1 English
+    ShowMe AAC 2.0
+    English goes live COMPACT - ACTIVEbook
+    Tecno Cloud ActiveBook
+    Pearson Longman
+    Priberam Dictionary of the Portuguese Language
+    Cradle of Persia
+    The Secret of Monkey Island: Special Edition
+    Feeding Frenzy 2
+    eText (A0102384122-MTVR-NO)
+    TRON: Evolution (PL/CZ)
+    cdBook (English)
